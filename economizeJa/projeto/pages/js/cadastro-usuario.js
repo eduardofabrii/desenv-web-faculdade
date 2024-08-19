ef691a028a3f3d85aed91ba1ff11b2c77d3ddaaa
Eduardo Fabri <eduardohfabri@gmail.com>
2024-08-19 19:58:50 -0300

    .

---
+++ economizeJa/projeto/pages/js/cadastro-usuario.js
@@ -0,0 +1,29 @@
+function validarLogin() {
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let cpf = document.getElementById('cpf').value;
+    let telefone = document.getElementById('telefone').value;
+
+    document.getElementById('campoObrigatorio').style.display = 'none';
+    document.getElementById('senhaTamanho').style.display = 'none';
+
+    if (!nome || !email || !senha || !cpf || !telefone) {
+        document.getElementById('campoObrigatorio').style.display = 'block';
+    }
+    
+    if (senha && senha.length < 8 || senha.length > 16) {
+       document.getElementById('senhaTamanho').style.display = 'block';
+    }
+}
+
+function validarPadraoSenha(event) {
+    let senha = event.target.value;
+    let regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).*$/;
+
+    if (!regex.test(senha)) {
+        document.getElementById('senhaPadrao').style.display = 'block';
+    } else {
+        document.getElementById('senhaPadrao').style.display = 'none';
+    }
+}
