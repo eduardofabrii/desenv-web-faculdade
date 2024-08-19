9c9798ef16056f8aff725a81b5672b61a8447617
Eduardo Fabri <eduardohfabri@gmail.com>
2024-08-19 14:43:51 -0300

    .

---
+++ economizeJa/projeto/js/teste.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('./pages'));
+
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+    res.send();
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
