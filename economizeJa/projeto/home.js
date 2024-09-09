356b7bdaad61f74bc13b43231da1ddbd0a004a04
Eduardo Fabri <eduardohfabri@gmail.com>
2024-09-09 19:39:48 -0300

    fazendo api

--- economizeJa/projeto/home.js
+++ economizeJa/projeto/home.js
@@ -1,7 +1,31 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+
 app.use(express.static('./pages'));
+
+const usuarios = [];
+
+const router = express.Router();
+router.get('/api/usuarios' , (req, res) => {
+    console.log('Entrou no GET')
+    res.status(200).json(usuarios);
+});
+
+router.post('/api/usuarios' , (req, res) => {
+    console.log('Entrou no POST')
+    console.log(req.body);
+
+    var usuario = req.body;
+    res.status(201).json;
+    usuario.id = 1;
+
+    usuarios.push(usuario);
+    res.status(201).json(usuarios);
+});
+
+app.use(router);
 
 const port = 3000;
 
