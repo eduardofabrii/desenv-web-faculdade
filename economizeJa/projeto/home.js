1e538b5939e1d05bfce2ed6fbef4324315bb5bfb
jvecodev <jvitor.oliveira1803@gmail.com>
2024-09-09 21:27:04 -0300

    .

--- economizeJa/projeto/home.js
+++ economizeJa/projeto/home.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+
 app.use(express.static('./pages'));
 
-const port = 3000;
+const produtos = [];
 
-app.get('/hello', (req, res) => {
-    res.send();
+const router = express.Router();
+router.get('/api/produtos', (req, res) => {  
+    console.log('entrou no get');
+    res.status(200).json(produtos);
+});
+router.post('/api/produtos', (req, res) => {  
+    console.log('entrou no post');
+    console.log(req.body);
+
+    var produto = req.body;
+    produto.id = 1;
+
+    produtos.push(produto);
+    res.status(201).json(produto);
 });
 
+app.use(router);
+
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`); 
+    console.log(`Server running at http://localhost:${port}`);
 });
