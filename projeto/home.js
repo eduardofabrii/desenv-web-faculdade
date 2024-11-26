8adb7140d0d5050d5471cd988a888a3c72e6e3ca
Eduardo Fabri <eduardohfabri@gmail.com>
2024-11-26 20:14:43 -0300

    Arrumando a ordem da conexao do banco

--- projeto/home.js
+++ projeto/home.js
@@ -51,9 +51,9 @@ const upload = multer({
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,        // Usa a variável de ambiente DB_HOST
     user: process.env.DB_USER,        // Usa a variável de ambiente DB_USER
-    password: process.env.DB_PASSWORD,// Usa a variável de ambiente DB_PASSWORD
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME     // Usa a variável de ambiente DB_NAME
-});
+});  
 
 connection.connect((err) => {
     if (err) {
